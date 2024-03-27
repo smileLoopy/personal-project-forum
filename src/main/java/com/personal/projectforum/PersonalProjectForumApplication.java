95d97bda6dd649323f72a115d1ff1693adbb9709
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-26 19:21:31 -0700

    #6 - Starting the Spring Boot Project
    Initializing spring boot project
    
    Included:
    * Spring web
    * Actuator
    * Devtools
    * Lombok

---
+++ src/main/java/com/personal/projectforum/PersonalProjectForumApplication.java
@@ -0,0 +1,13 @@
+package com.personal.projectforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalProjectForumApplication.class, args);
+    }
+
+}

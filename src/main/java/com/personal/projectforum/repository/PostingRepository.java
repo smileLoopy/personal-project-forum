6209744c4623e2ab6efbd4b4ff4992f113974653
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-30 08:30:31 -0700

    #1- Added API Search option with Querydsl
    Simply implement by using 'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer'.

--- src/main/java/com/personal/projectforum/repository/PostingRepository.java
+++ src/main/java/com/personal/projectforum/repository/PostingRepository.java
@@ -1,10 +1,30 @@
 package com.personal.projectforum.repository;
 
 import com.personal.projectforum.domain.Posting;
+import com.personal.projectforum.domain.QPosting;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PostingRepository extends JpaRepository<Posting, Long> {
+public interface PostingRepository extends
+        JpaRepository<Posting, Long>,
+        QuerydslPredicateExecutor<Posting>,
+        QuerydslBinderCustomizer<QPosting> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QPosting root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // query like '%${v}%
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // query like '%${v}%
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // query like '%${v}%
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // query like '%${v}%
+    }
 }
 

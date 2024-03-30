67f99397e2b7ae20642c2b803258b11c1057eb32
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-29 21:20:46 -0700

    #1- Disabled Spring Data Rest Test
    This test is not related to a business logic of this project. It's just a data rest function.
    Also since it's a integration test it's influencing DB performance. So disabled it.

--- src/test/java/com/personal/projectforum/controller/DataRestTest.java
+++ src/test/java/com/personal/projectforum/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.personal.projectforum.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST Integration test is not needed anymore")
 @DisplayName("DATA REST - API TEST")
 @Transactional
 @AutoConfigureMockMvc

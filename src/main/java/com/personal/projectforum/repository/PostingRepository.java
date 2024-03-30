dbf8ac322b6ddf93e724c886e2093481c14b8395
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-29 19:29:03 -0700

    #1- Setting data rest
    Setting it to make json api of posting, comments to be automatically be restful

--- src/main/java/com/personal/projectforum/repository/PostingRepository.java
+++ src/main/java/com/personal/projectforum/repository/PostingRepository.java
@@ -2,6 +2,8 @@ package com.personal.projectforum.repository;
 
 import com.personal.projectforum.domain.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface PostingRepository extends JpaRepository<Posting, Long> {
 }

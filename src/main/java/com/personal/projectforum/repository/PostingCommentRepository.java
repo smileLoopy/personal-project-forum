72b086c1e718b599155ea07762eb3adcab079b12
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-29 19:40:38 -0700

    #1-Refactoring: add EOF - try again

--- src/main/java/com/personal/projectforum/repository/PostingCommentRepository.java
+++ src/main/java/com/personal/projectforum/repository/PostingCommentRepository.java
@@ -7,3 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface PostingCommentRepository extends JpaRepository<PostingComment, Long> {
 }
+
+

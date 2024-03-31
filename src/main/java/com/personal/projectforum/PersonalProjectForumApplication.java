22706e764d5844f71fa1bbb319c9d27355f5bf96
smile.loopys2 <smile.loopys2@gmail.com>
2024-03-30 22:43:29 -0700

    #16- Thymeleaf decoupled logic setting
    Added settings to control thymeleaf3 decoupled logic with user application property in javacode.
    
    https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

--- src/main/java/com/personal/projectforum/PersonalProjectForumApplication.java
+++ src/main/java/com/personal/projectforum/PersonalProjectForumApplication.java
@@ -2,7 +2,9 @@ package com.personal.projectforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class PersonalProjectForumApplication {
 

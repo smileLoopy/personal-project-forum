05ea46047404aaedccff827e892b03153946bee4
smile_loopy <smile.loopys2@gmail.com>
2024-04-13 15:00:58 -0700

    #22- Applied the pagination function to controller(forum view)
    Since testing is using 'Page' class, testing the paging and sorting all together.
    Need to be continued when implementing it.

--- src/test/java/com/personal/projectforum/controller/PostingControllerTest.java
+++ src/test/java/com/personal/projectforum/controller/PostingControllerTest.java
@@ -3,6 +3,7 @@ package com.personal.projectforum.controller;
 import com.personal.projectforum.config.SecurityConfig;
 import com.personal.projectforum.dto.PostingWithCommentsDto;
 import com.personal.projectforum.dto.UserAccountDto;
+import com.personal.projectforum.service.PaginationService;
 import com.personal.projectforum.service.PostingService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -14,15 +15,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -36,6 +39,7 @@ class PostingControllerTest {
     private final MockMvc mvc;
 
     @MockBean private PostingService postingService;
+    @MockBean private PaginationService paginationService;
 
     public PostingControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -46,14 +50,46 @@ class PostingControllerTest {
     public void givenNothing_whenRequestingPostingsView_thenReturnsPostingsView() throws Exception {
         // Given
         given(postingService.searchPostings(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         // When & Then
         mvc.perform(get("/postings"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("postings/index"))
-                .andExpect(model().attributeExists("postings"));
+                .andExpect(model().attributeExists("postings"))
+                .andExpect(model().attributeExists("paginationBarNumbers"));
         then(postingService).should().searchPostings(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view][GET] posting list (forum) page - paging, sorting function")
+    @Test
+    void givenPagingAndSortingParams_whenSearchingPostingsPage_thenReturnsPostingsPage() throws Exception {
+        // Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1, 2, 3, 4, 5);
+        given(postingService.searchPostings(null, null, pageable)).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+        // When & Then
+        mvc.perform(
+                        get("/postings")
+                                .queryParam("page", String.valueOf(pageNumber))
+                                .queryParam("size", String.valueOf(pageSize))
+                                .queryParam("sort", sortName + "," + direction)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("postings/index"))
+                .andExpect(model().attributeExists("postings"))
+                .andExpect(model().attribute("paginationBarNumbers", barNumbers));
+        then(postingService).should().searchPostings(null, null, pageable);
+        then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
     @DisplayName("[view] [GET] Posting Detail Page - Normal Retrieval Case")

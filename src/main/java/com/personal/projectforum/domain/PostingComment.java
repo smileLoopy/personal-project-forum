e7b5b4726baa5661fb82d2f3f1a08889f636444b
smile_loopy <smile.loopys2@gmail.com>
2024-07-21 14:53:52 -0700

    #75- Design domain for child comment
    Added code to set parent and child relationships within the comment domian.
    
    Apply all cscading rules so that changes in the collection of child comments are reflected in the query.
    
    This time, we decided to use the one-way association setting. Therefore, the parent comment directly represents the long id rather than the entity.
    
    Additionally, an additional method is provided to add child comments.

--- src/main/java/com/personal/projectforum/domain/PostingComment.java
+++ src/main/java/com/personal/projectforum/domain/PostingComment.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -21,27 +23,42 @@ public class PostingComment extends AuditingFields{
     private Long id;
 
     @Setter
+    @ManyToOne(optional = false)
+    private Posting posting;
+
+    @Setter
     @JoinColumn(name = "userId")
     @ManyToOne(optional = false)
     private UserAccount userAccount;
 
-    @Setter 
-    @ManyToOne(optional = false)
-    private Posting posting;
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<PostingComment> childComments = new LinkedHashSet<>();
 
     @Setter @Column(nullable = false, length = 500) private String content;
 
     protected PostingComment() {} //It's possible to do it via lombok but since it's short just going to write it
 
     // Hide it with privte and open it with factory method
-    private PostingComment(Posting posting,  UserAccount userAccount, String content) {
+    private PostingComment(Posting posting,  UserAccount userAccount, Long parentCommentId, String content) {
         this.userAccount = userAccount;
         this.posting = posting;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
     // Factory method
     public static PostingComment of(Posting posting, UserAccount userAccount, String content) {
-        return new PostingComment(posting, userAccount, content);
+        return new PostingComment(posting, userAccount, null, content);
+    }
+
+    public void addChildComment(PostingComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
